2dd8e9526030c54a89666841bfe124740df92c38
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-09-13 18:10:49 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import BeersList from './components/beerslist/BeersList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BeersList />
     </div>
   );
 }
