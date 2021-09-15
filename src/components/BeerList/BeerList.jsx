bca31c1c655515dfe3fec1c91c565aa84a596a76
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-09-15 15:20:08 +0200

    Iteration 1 completed

---
+++ src/components/BeerList/BeerList.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function BeerList() {
+    return (
+        <div class="BeerList">
+            <p>Loading...</p>
+        </div>
+    )
+  
+}
+   
+    
