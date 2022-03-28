77d1b3c8b5092dbe9966d7a516d4030c33788a31
Arturo Vivar <arthas1237@hotmail.com>
2022-03-28 10:53:17 +0200

    navbardone

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return(
+        <div>
+            <nav className="navbar navbar-light bg-light">
+  <div className="container">
+    <a className="navbar-brand" href="#">
+      <img src="https://toppng.com/uploads/preview/house-png-address-logo-for-resume-11563644318qyw00houfc.png" alt="" width="30" height="24"/>
+    </a>
+  </div>
+</nav>
+
+        </div>
+    )
+}
+
+export default Navbar 
