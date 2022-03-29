2a55966783c010bdf215f1bba11befab6e51b7ce
Pedro Alberto Baños Folcra <pedroalbertobanosfolcra@MacBook-Pro-de-Pedro-2.local>
2022-03-29 12:44:42 +0200

    lab done w/ styles

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar navbar-light bg-info">
+            <div className="container-fluid justify-content-center">
+            <h1> <i className="fa-solid fa-house"/></h1>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar
