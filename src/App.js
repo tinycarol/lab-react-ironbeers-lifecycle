56d3a2231a06760faa34f4310b3e0ab9d1aa3902
Robert Cabrera <cabrera.rch@gmail.com>
2022-03-29 12:56:39 +0200

    Iteration 1 y 2 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { beerList } from './services/BeersService';
+import BeersList from './components/BeersList/BeersList';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state= {
+    beers : []
+  }
+
+  componentDidMount() {
+    beerList()
+      .then((response) => {
+        this.setState({
+          beers: response
+        })
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <BeersList />
+        {this.state.beers.map(beer => {
+          return (
+            <p>{beer.name}</p>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
