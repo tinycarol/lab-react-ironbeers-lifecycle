f2f9ec2b686f7cfee0f68f91cfa58fe33dcb7a70
Juan De Oliveira <juanabrahamd@gmail.com>
2022-09-27 13:12:34 +0200

    Bootstrap included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import BeerList from './Components/BeerList.jsx'
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
+    <NavBar />
     <BeerList></BeerList>
     </div>
   );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+
+    return (
+        <nav className="navbar-dark bg-primary">
+            <div className="logo">
+            <img src="https://coursereport-production.imgix.net/uploads/school/logo/84/original/logo-ironhack-blue.png?w=200&h=200&dpr=2&q=50"  height="55" width="50" class="d-inline-block align-top" alt="ironhack-logo" />
+            </div>
+        </nav>
+
+)
+}
+
+export default NavBar;
