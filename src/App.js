d9d0ea9aa18e5a7bb1bc6c45dcbcbdec8ea35cf9
violeta atkinson <violeta.atkinson@gmail.com>
2022-09-29 17:17:01 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import BeerList from './components/BeersList';
 
@@ -6,6 +7,9 @@ import BeerList from './components/BeersList';
 function App() {
   return (
     <div className="App">
+     <nav className="nav" >
+          <img src="/pngfind.com-white-house-logo-png-5898244.png" alt="logo" width={35}></img>
+     </nav>
     <BeerList/>
     </div>
   );
