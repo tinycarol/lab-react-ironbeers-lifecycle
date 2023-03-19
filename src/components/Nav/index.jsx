383135f8d44ca6e01791ed89a8bc1ee95d3835a8
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-03-19 23:23:42 +0100

    done

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css"
+import homeIcon from "../../assets/images/home.png"
+
+
+export const Nav = () => {
+    return (
+        <div className='nav'>
+            <nav > <img src={homeIcon}></img></nav>
+        </div>
+    )
+}
+export default Nav
